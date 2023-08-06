754376f25c5d0bcff0713d260af394702debfd5d
AlexandrOvsii <120751762+AlexandrOvsii@users.noreply.github.com>
2023-08-06 18:24:49 +0000

    Deploying to gh-pages from @ AlexandrOvsii/goit-js-hw-09@0fbb3eb9db9e0c9243ad91a850a41e929420f215 🚀

---
+++ 01-color-switcher.386aa41d.js
@@ -0,0 +1,2 @@
+const t={startButton:document.querySelector("[data-start]"),stopButton:document.querySelector("[data-stop]")};t.startButton.addEventListener("click",(function(){null===n&&(n=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3));t.startButton.disabled=!0,t.stopButton.disabled=!1})),t.stopButton.addEventListener("click",(function(){null!==setInterval&&(clearInterval(n),n=null);t.startButton.disabled=!1,t.stopButton.disabled=!0}));let n=null;
+//# sourceMappingURL=01-color-switcher.386aa41d.js.map
