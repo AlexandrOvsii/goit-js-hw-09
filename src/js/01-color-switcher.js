0fbb3eb9db9e0c9243ad91a850a41e929420f215
Alexandr Ovsii <sedanvw@gmail.com>
2023-08-06 21:24:23 +0300

    color-switcher-done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
+const refs = {
+  startButton: document.querySelector('[data-start]'),
+  stopButton: document.querySelector('[data-stop]'),
+};
 
+refs.startButton.addEventListener('click', startColorChange);
+refs.stopButton.addEventListener('click', stopColorChange);
+
+let intervalId = null;
+
+function startColorChange() {
+  if (intervalId === null) {
+    intervalId = setInterval(() => {
+      document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+  }
+
+  refs.startButton.disabled = true;
+  refs.stopButton.disabled = false;
+}
+
+function stopColorChange() {
+  if (setInterval !== null) {
+    clearInterval(intervalId);
+    intervalId = null;
+  }
+  refs.startButton.disabled = false;
+  refs.stopButton.disabled = true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
