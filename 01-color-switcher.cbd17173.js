de3d395c009c468a72382b52e2de06898bfdf07f
AlexandrOvsii <120751762+AlexandrOvsii@users.noreply.github.com>
2023-09-26 06:42:22 +0000

    Deploying to gh-pages from @ AlexandrOvsii/goit-js-hw-09@140cc48d37e8da65b2d563e54e5f1eb3e7143e72 🚀

---
+++ 01-color-switcher.cbd17173.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(function(){null===a&&(a=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`,t.classList.add("disabled"),e.classList.remove("disabled")}),1e3))})),e.addEventListener("click",(function(){null!==a&&(clearInterval(a),t.classList.remove("disabled"),e.classList.add("disabled"))})),e.classList.add("disabled");let a=null;
+//# sourceMappingURL=01-color-switcher.cbd17173.js.map
